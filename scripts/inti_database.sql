6c7b0fc9bdadfba9bb597d8805e92df1faa648a1
spikeleesjoint01 <jhm135@scarletmail.rutgers.edu>
2026-04-19 22:49:24 -0400

    Create inti_database.sql

---
+++ scripts/inti_database.sql
@@ -0,0 +1,13 @@
+
+--Create Database and Schemas*/
+USE master;
+
+CREATE DATABASE DataWarehouse;
+USE DataWarehouse;
+
+CREATE SCHEMA BRONZE;
+GO
+CREATE SCHEMA SILVER;
+GO
+CREATE SCHEMA GOLD;
+GO
